ed72a8a3f0bc6977b7037a22aecd63fbac238d67
Roman <gobkins@gmail.com>
2019-08-20 18:59:43 -0400

    basic functionality, picking options, scoring and outcomes working

--- script.js
+++ script.js
@@ -6,81 +6,121 @@ $(function(){
     }
     const room0 = {
         image:"https://placebeard.it/640x360",
-        text:"Such text wow.",
+        text:"Such text from room0 wow.",
         options: [
             {
                 optionText:"option1 veri much",
-                outcomeText:"poor life choices lead to this",
-                roomOutcome: 3
+                outcomeText:"poor life choices led to this",
+                roomOutcome: 1,
+                scoreChange: 1
             },
             {
                 optionText:"option2 veri much",
-                outcomeText:"poor life choices lead to this",
-                roomOutcome: 3
+                outcomeText:"medium life choices led to this",
+                roomOutcome: 2,
+                scoreChange: 1
             },
             {
                 optionText:"option3 veri much",
-                outcomeText:"poor life choices lead to this",
-                roomOutcome: 3
+                outcomeText:"succes life choices led to this",
+                roomOutcome: 1,
+                scoreChange: 1
             }
         ]
     }
     const room1 = {
         image:"https://placebeard.it/640x360",
-        text:"Such text wow.",
+        text:"Such text from room1 wow.",
         options: [
             {
                 optionText:"option1 veri much",
-                outcomeText:"poor life choices lead to this",
-                roomOutcome: 3
+                outcomeText:"poor life choices led to this",
+                roomOutcome: 3,
+                scoreChange: 1
             },
             {
                 optionText:"option2 veri much",
-                outcomeText:"poor life choices lead to this",
-                roomOutcome: 3
+                outcomeText:"medium life choices led to this",
+                roomOutcome: 3,
+                scoreChange: 1
             },
             {
                 optionText:"option3 veri much",
-                outcomeText:"poor life choices lead to this",
-                roomOutcome: 3
+                outcomeText:"success life choices led to this",
+                roomOutcome: 3,
+                scoreChange: 1
             }
         ]
     }
     const room2 = {
         image:"https://placebeard.it/640x360",
-        text:"Such text wow.",
+        text:"Such text from room2 wow.",
         options: [
             {
                 optionText:"option1 veri much",
-                outcomeText:"poor life choices lead to this",
-                roomOutcome: 3
+                outcomeText:"poor life choices led to this",
+                roomOutcome: 3,
+                scoreChange: 1
             },
             {
                 optionText:"option2 veri much",
-                outcomeText:"poor life choices lead to this",
-                roomOutcome: 3
+                outcomeText:"medium life choices led to this",
+                roomOutcome: 3,
+                scoreChange: 1
             },
             {
                 optionText:"option3 veri much",
-                outcomeText:"poor life choices lead to this",
-                roomOutcome: 3
+                outcomeText:"success life choices led to this",
+                roomOutcome: 3,
+                scoreChange: 1
             }
         ]
     }
 
     const roomsArray = [room0, room1, room2];
+    let currentRoom = roomsArray[character.currentLocation];
     
     // Display initial text, options and picture;
-    $('img').attr('src', roomsArray[character.currentLocation].image);
-    $('h1').text(room0.text);
-    for (let i = 0; i < 3; i++){
-        $(`.option${i}`).text(roomsArray[character.currentLocation].options[i].optionText);
+    
+    const checkAnswer = function(){
+        let userAnswer = $(this).text();
+        let pickedOption = currentRoom.options.find( answer => answer.optionText === userAnswer);
+        $('h1').text(pickedOption.outcomeText);
+        $('button').toggleClass('hidden');
+        $('li').off('click');
+        // Increment/decrement score;
+        character.score = character.score + pickedOption.scoreChange;
+        character.currentLocation = pickedOption.roomOutcome;
+        console.log(character.currentLocation);
+        console.log(character);
+        console.log(currentRoom);
+        currentRoom = roomsArray[character.currentLocation];
     }
+
+    const populate = () =>{
+
+        $('img').attr('src', currentRoom.image);
+        $('h1').text(currentRoom.text);
+        $('.score').text(character.score);
+        for (let i = 0; i < 3; i++){
+            $(`.option${i}`).text(currentRoom.options[i].optionText);
+        }
+        $('li').on('click', checkAnswer);
+        $('button').toggleClass('hidden');
+    }
+
+    $('button').toggleClass('hidden');
+    populate();
+
+    
+    // Listen to which option the user picks from the list;
+    // Match the selected input with the corresponding result;
+    // Display the result of user’s choice;
+    
+
+    
+    $('button').on('click', populate);
 });
-// Listen to which option the user picks from the list;
-// Match the selected input with the corresponding result;
-// Display the result of user’s choice;
-// Increment/decrement score;
 // Check if the score is too low;
 // Display text, options and picture of the next part based on the previous choice;
 // Repeat till the end;
